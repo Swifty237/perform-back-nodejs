6f0cc36b376cd3a649e7547d3642e400467185ad
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-20 00:32:46 +0200

    update server.js buid app configuration

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(jsonParser);
 app.use(express.static(__dirname));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.LOCAL_FRONT_ACCESS_URI);
+    res.header("Access-Control-Allow-Origin", process.env.LOCAL_FRONT_ACCESS_URI ? process.env.LOCAL_FRONT_ACCESS_URI : process.env.PROD_FRONT_ACCESS_URI);
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
     res.header(
         "Access-Control-Allow-Headers",
