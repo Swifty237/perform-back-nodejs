2a6ac8c6c80d29adb5ff3e59aacd910d8c2d5df2
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-25 14:17:08 +0200

    refactoring commit

--- ufc-news-routes.js
+++ ufc-news-routes.js
@@ -1,25 +1,23 @@
 import express from 'express';
-import getMongoConnection from './utils/database.js';
-// import mongoose from 'mongoose';
+import getMongooseConnection from './utils/database.js';
+import ufcNewsDataSchema from './models/ufc-news-model.js'
 import dotenv from 'dotenv';
 
 dotenv.config();
-const db = getMongoConnection();
+getMongooseConnection();
 
 const apiRouter = express.Router();
 
 const getUfcNews = async () => {
 
     try {
-        const ufcNewsCollection = db.collection("ufcnews");
-        const arrayNews = await ufcNewsCollection.find().toArray();
+        const arrayNews = await ufcNewsDataSchema.find();
         return arrayNews;
 
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
         res.status(500).json({ error: "Erreur serveur" });
     }
-
 }
 
 apiRouter.route('/performmma/ufcnews').
